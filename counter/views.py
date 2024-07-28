2f6ef7389a64a93dc50a05761130af4f11947522
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-07-27 19:54:56 -0500

    added error page

--- counter/views.py
+++ counter/views.py
@@ -15,6 +15,6 @@ def home(request):
             api = json.loads(api_request.content)
         except Exception as e:
             api = "oops! something went wrong"
-        return render(request, 'home.html', {'api': api})
+        return render(request, '500.html', {'api': api})
     else:
-        return render(request, 'home.html', {'query': 'Enter a food item to get its nutrition details'})
+        return render(request, 'error.html')
